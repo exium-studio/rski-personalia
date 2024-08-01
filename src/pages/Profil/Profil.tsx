02f191a8fcd76fbe84e3e32c704c8d397da758ab
sulenq <whyimustwriteit@gmail.com>
2024-08-01 10:23:29 +0700

    styling

--- src/pages/Profil/Profil.tsx
+++ src/pages/Profil/Profil.tsx
@@ -20,11 +20,7 @@ import { removeCookie } from "typescript-cookie";
 import DisclosureHeader from "../../components/dependent/DisclosureHeader";
 import CContainer from "../../components/wrapper/CContainer";
 import CWrapper from "../../components/wrapper/CWrapper";
-import {
-  useDarkLightColor,
-  useErrorAlphaColor,
-  useLightDarkColor,
-} from "../../constant/colors";
+import { useLightDarkColor } from "../../constant/colors";
 import { iconSize, responsiveSpacing } from "../../constant/sizes";
 import useBackOnClose from "../../hooks/useBackOnClose";
 import useGetUserData from "../../hooks/useGetUserData";
@@ -38,7 +34,6 @@ const LogoutConfirmation = ({ ...props }: LogoutProps) => {
   const navigate = useNavigate();
 
   // SX
-  const errorAlphaColor = useErrorAlphaColor();
 
   return (
     <>
@@ -53,7 +48,6 @@ const LogoutConfirmation = ({ ...props }: LogoutProps) => {
         colorScheme="red"
         variant={"ghost"}
         size={"lg"}
-        bg={errorAlphaColor}
         onClick={onOpen}
         className="clicky"
         {...props}
@@ -104,108 +98,122 @@ const LogoutConfirmation = ({ ...props }: LogoutProps) => {
 export default function Profil() {
   // SX
   const lightDarkColor = useLightDarkColor();
-  const darkLightColor = useDarkLightColor();
 
   const userData = useGetUserData();
 
   return (
-    <CWrapper pt={0} overflowY={"auto"}>
-      <Box w={"50px"} flex={"1 1 50px"} bg={"#353535"} mx={"auto"} />
+    <CWrapper pt={0}>
+      <>
+        <Box w={"50px"} flex={"1 0 50px"} bg={"#353535"} mx={"auto"} />
 
-      <Box
-        w={"65px"}
-        h={"20px"}
-        bg={"#353535"}
-        mx={"auto"}
-        p={1}
-        borderRadius={"full"}
-      >
-        <Box w={"100%"} h={"100%"} bg={lightDarkColor} borderRadius={"full"} />
-      </Box>
-
-      <Box
-        mt={"-5px"}
-        mb={"-10px"}
-        w={"16px"}
-        h={"30px"}
-        bg={"gray"}
-        mx={"auto"}
-        borderRadius={"2px"}
-        zIndex={3}
-      />
-
-      <Box
-        w={"200px"}
-        h={"16px"}
-        bg={lightDarkColor}
-        mx={"auto"}
-        p={1}
-        borderRadius={"100px 100px 0 0"}
-        border={"1px solid var(--divider3)"}
-        borderBottom={"none !important"}
-        zIndex={2}
-        mb={"-2px"}
-      >
         <Box
-          mt={"2px"}
-          w={"50px"}
-          h={"8px"}
-          bg={darkLightColor}
-          borderRadius={"full"}
-          opacity={0.1}
+          flexShrink={0}
+          w={"65px"}
+          h={"20px"}
+          bg={"#353535"}
           mx={"auto"}
+          p={1}
+          borderRadius={"full"}
+        >
+          <Box
+            w={"100%"}
+            h={"100%"}
+            bg={lightDarkColor}
+            borderRadius={"full"}
+          />
+        </Box>
+
+        <Box
+          flexShrink={0}
+          mt={"-5px"}
+          mb={"-10px"}
+          w={"16px"}
+          h={"30px"}
+          bg={"gray"}
+          mx={"auto"}
+          borderRadius={"2px"}
+          zIndex={3}
         />
-      </Box>
+
+        <Box
+          flexShrink={0}
+          w={"200px"}
+          h={"16px"}
+          bg={"p.500"}
+          mx={"auto"}
+          p={1}
+          borderRadius={"100px 100px 0 0"}
+          border={"1px solid var(--divider3)"}
+          borderBottom={"none !important"}
+          zIndex={2}
+          mb={"-2px"}
+        >
+          <Box
+            mt={"2px"}
+            w={"50px"}
+            h={"8px"}
+            bg={"dark"}
+            borderRadius={"full"}
+            mx={"auto"}
+            opacity={0.4}
+          />
+        </Box>
+      </>
 
       <Box m={"auto"} flex={5}>
-        <CContainer
-          bg={lightDarkColor}
-          borderRadius={12}
-          position={"relative"}
-          overflowY={"auto"}
-          className="scrollY admin-card"
-          w={"300px"}
-          mb={responsiveSpacing}
-          p={2}
-          border={"1px solid var(--divider3)"}
-        >
-          <CContainer flex={"1 1 150px"} gap={responsiveSpacing}>
-            <Image
-              src={userData?.foto_profil || "/images/defaultProfilePhoto.webp"}
-              aspectRatio={1}
-              borderRadius={"8px 8px 0 0"}
-              boxSize={"281.63px"}
-            />
-          </CContainer>
-
+        <Box position={"relative"}>
           <CContainer
-            p={responsiveSpacing}
             bg={"p.500"}
-            color={"white"}
-            borderRadius={"0 0 8px 8px"}
+            borderRadius={12}
             position={"relative"}
-            overflow={"clip"}
+            overflowY={"auto"}
+            className="admin-card"
+            w={"300px"}
+            mb={responsiveSpacing}
+            p={1}
+            border={"1px solid var(--divider3)"}
           >
-            <Text fontWeight={600} fontSize={20}>
-              {userData.nama}
-            </Text>
+            <CContainer flex={"1 1 150px"} gap={responsiveSpacing}>
+              <Image
+                src={
+                  userData?.foto_profil || "/images/defaultProfilePhoto.webp"
+                }
+                aspectRatio={1}
+                borderRadius={"8px 8px 0 0"}
+                boxSize={"289.61x"}
+              />
+            </CContainer>
 
-            <Text opacity={0.4}>{userData.role.name}</Text>
+            <CContainer
+              p={responsiveSpacing}
+              bg={"white"}
+              color={"dark"}
+              borderRadius={"0 0 8px 8px"}
+              border={"1px solid var(--divider3)"}
+              position={"relative"}
+              overflow={"clip"}
+            >
+              <Text fontWeight={600} fontSize={20}>
+                {userData.nama}
+              </Text>
 
-            <HStack align={"end"} mt={12}>
-              <Text fontSize={14}>{userData?.email}</Text>
-            </HStack>
+              <Text opacity={0.4}>{userData.role.name}</Text>
 
-            <Image
-              src="/images/logoWhite.svg"
-              position={"absolute"}
-              bottom={-5}
-              right={-5}
-              w={"150px"}
-              opacity={0.1}
-            />
+              <HStack align={"end"} mt={12}>
+                <Text fontSize={14}>{userData?.email}</Text>
+              </HStack>
+
+              <Image
+                src="/logo512.png"
+                position={"absolute"}
+                bottom={-16}
+                right={-16}
+                w={"200px"}
+                opacity={0.1}
+              />
+            </CContainer>
           </CContainer>
-        </CContainer>
+        </Box>
 
         <LogoutConfirmation w={"100%"} mb={"auto"} />
       </Box>
