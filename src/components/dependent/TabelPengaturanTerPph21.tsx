6e3d54cc3e2333f3ad71da427b6cb7fdcb90ec75
sulenq <whyimustwriteit@gmail.com>
2024-08-01 15:00:10 +0700

    styling

--- src/components/dependent/TabelPengaturanTerPph21.tsx
+++ src/components/dependent/TabelPengaturanTerPph21.tsx
@@ -1,15 +1,17 @@
-import { Center, HStack, Icon, MenuItem, Text } from "@chakra-ui/react";
+import { Center, Icon, MenuItem, Text } from "@chakra-ui/react";
 import { RiDeleteBinLine, RiEditLine, RiHistoryLine } from "@remixicon/react";
-import { dummyTerPph21 } from "../../const/dummy";
-import { iconSize, responsiveSpacing } from "../../constant/sizes";
 import { Interface__SelectOption } from "../../constant/interfaces";
+import { iconSize } from "../../constant/sizes";
 import useDataState from "../../hooks/useDataState";
 import formatNumber from "../../lib/formatNumber";
+import isObjectEmpty from "../../lib/isObjectEmpty";
 import NoData from "../independent/NoData";
 import NotFound from "../independent/NotFound";
 import Skeleton from "../independent/Skeleton";
 import CustomTableContainer from "../wrapper/CustomTableContainer";
 import CustomTable from "./CustomTable";
+import DeleteDataPengaturanModalDisclosure from "./DeleteDataPengaturanModalDisclosure";
+import RestoreDataPengaturanModalDisclosure from "./RestoreDataPengaturanModalDisclosure";
 import Retry from "./Retry";
 import StatusDihapus from "./StatusDihapus";
 
@@ -32,26 +34,39 @@ export default function TabelPengaturanTerPph21({ filterConfig }: Props) {
     },
     (rowData: any) => {
       return (
-        <MenuItem isDisabled={!rowData.columnsFormat[1].value}>
-          <Text>Restore</Text>
-          <Icon as={RiHistoryLine} fontSize={iconSize} opacity={0.4} />
-        </MenuItem>
+        <RestoreDataPengaturanModalDisclosure
+          id={rowData.id}
+          url="/api/rski/dashboard/pengaturan/ter-pph-21/restore"
+        >
+          <MenuItem isDisabled={!rowData.columnsFormat[1].value}>
+            <Text>Restore</Text>
+            <Icon as={RiHistoryLine} fontSize={iconSize} opacity={0.4} />
+          </MenuItem>
+        </RestoreDataPengaturanModalDisclosure>
       );
     },
     "divider",
     (rowData: any) => {
       return (
-        <MenuItem fontWeight={500} isDisabled={rowData.columnsFormat[1].value}>
-          <Text color={"red.400"}>Delete</Text>
-          <Icon color={"red.400"} as={RiDeleteBinLine} fontSize={iconSize} />
-        </MenuItem>
+        <DeleteDataPengaturanModalDisclosure
+          id={rowData.id}
+          url="/api/rski/dashboard/pengaturan/ter-pph-21"
+        >
+          <MenuItem
+            fontWeight={500}
+            isDisabled={rowData.columnsFormat[1].value}
+          >
+            <Text color={"red.400"}>Delete</Text>
+            <Icon color={"red.400"} as={RiDeleteBinLine} fontSize={iconSize} />
+          </MenuItem>
+        </DeleteDataPengaturanModalDisclosure>
       );
     },
   ];
 
-  const { error, loading, data, retry } = useDataState<any[]>({
-    initialData: dummyTerPph21,
-    url: "",
+  const { error, notFound, loading, data, retry } = useDataState<any[]>({
+    initialData: undefined,
+    url: "/api/rski/dashboard/pengaturan/ter-pph-21",
     dependencies: [],
   });
 
@@ -61,7 +76,7 @@ export default function TabelPengaturanTerPph21({ filterConfig }: Props) {
       (term: Interface__SelectOption) => term.value
     );
 
-    const matchesSearchTerm = item.ptkp.kode_ptkp
+    const matchesSearchTerm = item.ptkp?.kode_ptkp
       .toLowerCase()
       .includes(searchTerm);
     const matchesIsDeletedTerm =
@@ -111,12 +126,12 @@ export default function TabelPengaturanTerPph21({ filterConfig }: Props) {
       },
     },
   ];
-  const formattedData = fd?.map((item: any) => ({
+  const formattedData = data?.map((item: any) => ({
     id: item.id,
     columnsFormat: [
       {
-        value: item.ptkp.kode_ptkp,
-        td: item.ptkp.kode_ptkp,
+        value: item.ptkp?.kode_ptkp,
+        td: item.ptkp?.kode_ptkp,
         isSortable: true,
         props: {
           position: "sticky",
@@ -156,20 +171,26 @@ export default function TabelPengaturanTerPph21({ filterConfig }: Props) {
   return (
     <>
       {error && (
-        <Center my={"auto"} minH={"400px"}>
-          <Retry loading={loading} retry={retry} />
-        </Center>
+        <>
+          {notFound && isObjectEmpty(filterConfig) && <NoData minH={"400px"} />}
+
+          {notFound && !isObjectEmpty(filterConfig) && (
+            <NotFound minH={"400px"} />
+          )}
+
+          {!notFound && (
+            <Center my={"auto"} minH={"400px"}>
+              <Retry loading={loading} retry={retry} />
+            </Center>
+          )}
+        </>
       )}
+
       {!error && (
         <>
           {loading && (
             <>
               <Skeleton minH={"300px"} flex={1} mx={"auto"} />
-              <HStack justify={"space-between"} mt={responsiveSpacing}>
-                <Skeleton maxW={"120px"} />
-                <Skeleton maxW={"300px"} h={"20px"} />
-                <Skeleton maxW={"112px"} />
-              </HStack>
             </>
           )}
           {!loading && (
