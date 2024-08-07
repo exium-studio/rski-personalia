cd911d93a1defde82ed58057ef8959084e3741b5
sulenq <whyimustwriteit@gmail.com>
2024-08-07 16:09:46 +0700

    styling

--- src/components/independent/TerapkanJadwalModal.tsx
+++ src/components/independent/TerapkanJadwalModal.tsx
@@ -97,6 +97,7 @@ export default function TerapkanJadwalModal({ ...props }: Props) {
                   }}
                   inputValue={formik.values.list_karyawan}
                   isError={!!formik.errors.list_karyawan}
+                  optionsDisplay="chip"
                 />
                 <FormErrorMessage>
                   {formik.errors.list_karyawan as string}
