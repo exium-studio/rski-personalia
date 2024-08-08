1f16a8e6b7baaa84d6b2e42a3dde95c783273890
sulenq <whyimustwriteit@gmail.com>
2024-08-08 22:07:50 +0700

    styling

--- src/components/dependent/PleaseWaitModal.tsx
+++ src/components/dependent/PleaseWaitModal.tsx
@@ -22,15 +22,21 @@ export default function PleaseWaitModal({ isOpen }: Props) {
       closeOnEsc={false}
       closeOnOverlayClick={false}
     >
-      <ModalOverlay />
+      <ModalOverlay bg={"#191919dd"} />
       <ModalContent bg={"transparent"} backdropFilter={"none"} border={"none"}>
         <ModalBody
           py={6}
           aspectRatio={1}
           justifyContent={"center"}
-          // color={"white"}
+          color={"white"}
         >
-          <Spinner w={"120px"} h={"120px"} mx={"auto"} mb={6} />
+          <Spinner
+            w={"120px"}
+            h={"120px"}
+            mx={"auto"}
+            mb={6}
+            borderWidth={"4px"}
+          />
           <Text textAlign={"center"} fontSize={20}>
             Mohon tunggu sebentar, proses ini mungkin memerlukan beberapa saat
             untuk selesai.
