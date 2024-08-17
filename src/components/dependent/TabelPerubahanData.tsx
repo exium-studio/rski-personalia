a1c732a63fcf2362311bd4232dee903680d285c9
sulenq <whyimustwriteit@gmail.com>
2024-08-17 17:19:11 +0700

    styling

--- src/components/dependent/TabelPerubahanData.tsx
+++ src/components/dependent/TabelPerubahanData.tsx
@@ -1,18 +1,74 @@
-import { Center, HStack } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useState } from "react";
-import { responsiveSpacing } from "../../constant/sizes";
-import perubahanDataKolom from "../../constant/perubahanDataKolom";
 import useFilterKaryawan from "../../global/useFilterKaryawan";
 import useDataState from "../../hooks/useDataState";
+import isObjectEmpty from "../../lib/isObjectEmpty";
 import NoData from "../independent/NoData";
+import NotFound from "../independent/NotFound";
 import Skeleton from "../independent/Skeleton";
 import CustomTableContainer from "../wrapper/CustomTableContainer";
 import AvatarAndNameTableData from "./AvatarAndNameTableData";
-import BooleanBadge from "./BooleanBadge";
 import CustomTable from "./CustomTable";
 import PerubahanDataRender from "./PerubahanDataRender";
 import Retry from "./Retry";
+import StatusApprovalBadge from "./StatusApprovalBadge";
 import TabelFooterConfig from "./TabelFooterConfig";
+import useBackOnClose from "../../hooks/useBackOnClose";
+import backOnClose from "../../lib/backOnClose";
+import DisclosureHeader from "./DisclosureHeader";
+
+interface KonfirmasiProps {
+  data: any;
+}
+
+function KonfirmasiPermintaan({ data }: KonfirmasiProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  useBackOnClose(
+    `konfirmasi-permintaan-perubahab-data-${data.id}`,
+    isOpen,
+    onOpen,
+    onClose
+  );
+
+  return (
+    <>
+      <Button
+        className="clicky"
+        colorScheme="ap"
+        variant={"ghost"}
+        onClick={onOpen}
+      >
+        Konfirmasi
+      </Button>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={backOnClose}
+        isCentered
+        blockScrollOnMount={false}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <DisclosureHeader title={"Konfirmasi Permintaan"} />
+          </ModalHeader>
+          <ModalBody></ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 
 export default function TabelVerifikasiData() {
   // Limit Config
@@ -20,67 +76,16 @@ export default function TabelVerifikasiData() {
   // Pagination Config
   const [pageConfig, setPageConfig] = useState<number>(1);
   // Filter Config
-  const { filterKaryawan } = useFilterKaryawan();
+  const { formattedFilterKaryawan } = useFilterKaryawan();
 
-  const dummy = [
-    {
-      user_id: 1,
-      user: {
-        id: 1,
-        nama: "User 1",
-        username: "username1",
-        email_verified_at: null,
-        role_id: null,
-        foto_profil: null,
-        data_completion_step: 1,
-        status_akun: 0,
-        created_at: "2024-06-11T04:39:19.000000Z",
-        updated_at: "2024-06-11T04:39:19.000000Z",
-      },
-      content: {
-        id: 1,
-        kolom: "tgl_lahir",
-        original_data: "2001-11-01",
-        updated_data: "2001-11-05",
-        status_perubahan: true,
-        created_at: "2024-07-10",
-        updated_at: "2024-07-11",
-      },
-      created_at: "2023-05-02",
-    },
-    {
-      user_id: 2,
-      user: {
-        id: 2,
-        nama: "User 2",
-        username: "username2",
-        email_verified_at: null,
-        role_id: null,
-        foto_profil: null,
-        data_completion_step: 1,
-        status_akun: 0,
-        created_at: "2024-06-11T04:39:19.000000Z",
-        updated_at: "2024-06-11T04:39:19.000000Z",
-      },
-      content: {
-        id: 2,
-        kolom: "foto_profil",
-        original_data: "https://bit.ly/dan-abramov",
-        updated_data: "/images/gear5.jpg",
-        status_perubahan: false,
-        created_at: "2024-07-22",
-        updated_at: "2024-07-23",
-      },
-      created_at: "2024-07-22",
-    },
-  ];
-
-  const { error, loading, data, retry } = useDataState<any>({
-    initialData: dummy,
-    url: "",
-    payload: filterKaryawan,
-    dependencies: [],
-  });
+  const { error, notFound, loading, data, paginationData, retry } =
+    useDataState<any>({
+      initialData: undefined,
+      url: `/api/rski/dashboard/karyawan/riwayat-perubahan/get-riwayat-perubahan-karyawan?page=${pageConfig}`,
+      payload: { ...formattedFilterKaryawan },
+      limit: limitConfig,
+      dependencies: [limitConfig, pageConfig, formattedFilterKaryawan],
+    });
 
   const formattedHeader = [
     {
@@ -106,9 +111,6 @@ export default function TabelVerifikasiData() {
     {
       th: "Kolom",
       isSortable: true,
-      cProps: {
-        justify: "center",
-      },
     },
     {
       th: "Data Original",
@@ -120,6 +122,18 @@ export default function TabelVerifikasiData() {
       th: "Data Pengajuan",
       cProps: {
         justify: "center",
+      },
+    },
+    {
+      th: "",
+      props: {
+        position: "sticky",
+        right: 0,
+        zIndex: 2,
+      },
+      cProps: {
+        justify: "center",
+        borderLeft: "1px solid var(--divider3)",
       },
     },
   ];
@@ -147,35 +161,25 @@ export default function TabelVerifikasiData() {
         },
       },
       {
-        value: item.content.status_perubahan,
+        value: item?.status_perubahan?.id,
         td: (
-          <BooleanBadge
-            w={"150px"}
-            data={item.content.status_perubahan}
-            trueValue="Diverifikasi"
-            falseValue="Verifikasi Ditolak"
-            nullValue="Menunggu"
-          />
+          <StatusApprovalBadge data={item?.status_perubahan.id} w={"120px"} />
         ),
+        isNumeric: true,
         cProps: {
           justify: "center",
         },
       },
       {
-        value: item.content.status_perubahan,
-        //@ts-ignore
-        td: perubahanDataKolom[item.content.kolom],
-
-        cProps: {
-          justify: "center",
-        },
+        value: item.kolom,
+        td: item.kolom,
       },
       {
         value: item.data,
         td: (
           <PerubahanDataRender
-            column={item.content.kolom}
-            data={item.content.original_data}
+            column={item.kolom?.toLowerCase()}
+            data={item.original_data}
           />
         ),
         cProps: {
@@ -186,12 +190,25 @@ export default function TabelVerifikasiData() {
         value: item.data,
         td: (
           <PerubahanDataRender
-            column={item.content.kolom}
-            data={item.content.updated_data}
+            column={item.kolom?.toLowerCase()}
+            data={item.updated_data}
           />
         ),
         cProps: {
           justify: "center",
+        },
+      },
+      {
+        value: "",
+        td: <KonfirmasiPermintaan data={item} />,
+        props: {
+          position: "sticky",
+          right: 0,
+          zIndex: 2,
+        },
+        cProps: {
+          justify: "center",
+          borderLeft: "1px solid var(--divider3)",
         },
       },
     ],
@@ -200,20 +217,27 @@ export default function TabelVerifikasiData() {
   return (
     <>
       {error && (
-        <Center my={"auto"} minH={"300px"}>
-          <Retry loading={loading} retry={retry} />
-        </Center>
+        <>
+          {notFound && isObjectEmpty(formattedFilterKaryawan) && (
+            <NoData minH={"300px"} />
+          )}
+
+          {notFound && !isObjectEmpty(formattedFilterKaryawan) && (
+            <NotFound minH={"300px"} />
+          )}
+
+          {!notFound && (
+            <Center my={"auto"} minH={"300px"}>
+              <Retry loading={loading} retry={retry} />
+            </Center>
+          )}
+        </>
       )}
       {!error && (
         <>
           {loading && (
             <>
               <Skeleton minH={"300px"} flex={1} mx={"auto"} />
-              <HStack justify={"space-between"} mt={responsiveSpacing}>
-                <Skeleton maxW={"120px"} />
-                <Skeleton maxW={"300px"} h={"20px"} />
-                <Skeleton maxW={"112px"} />
-              </HStack>
             </>
           )}
           {!loading && (
@@ -233,29 +257,20 @@ export default function TabelVerifikasiData() {
                       // }}
                     />
                   </CustomTableContainer>
-
-                  <TabelFooterConfig
-                    limitConfig={limitConfig}
-                    setLimitConfig={setLimitConfig}
-                    pageConfig={pageConfig}
-                    setPageConfig={setPageConfig}
-                    paginationData={{
-                      prev_page_url: "",
-                      next_page_url: "",
-                      last_page: 1,
-                    }}
-                    // footer={
-                    //   <Text opacity={0.4}>
-                    //     Klik row untuk melihat detail karyawan
-                    //   </Text>
-                    // }
-                  />
                 </>
               )}
             </>
           )}
         </>
       )}
+
+      <TabelFooterConfig
+        limitConfig={limitConfig}
+        setLimitConfig={setLimitConfig}
+        pageConfig={pageConfig}
+        setPageConfig={setPageConfig}
+        paginationData={paginationData}
+      />
     </>
   );
 }
