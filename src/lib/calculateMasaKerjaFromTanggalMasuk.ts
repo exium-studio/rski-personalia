7b7de647043218cb81946ae88fd221b0313b7af8
sulenq <whyimustwriteit@gmail.com>
2024-08-27 19:47:30 +0700

    styling

--- src/lib/calculateMasaKerjaFromTanggalMasuk.ts
+++ src/lib/calculateMasaKerjaFromTanggalMasuk.ts
@@ -1,35 +1,41 @@
+import isDatePassed from "./isDatePassed";
+
 export default function calculateMasaKerjaFromTanggalMasuk(dateString: string) {
   if (dateString) {
-    // Parse the input date string (format: d-m-y)
-    const [day, month, year] = dateString?.split("-")?.map(Number);
-    const startDate = new Date(year, month - 1, day);
+    if (isDatePassed(dateString, true)) {
+      return "-";
+    } else {
+      // Parse the input date string (format: d-m-y)
+      const [day, month, year] = dateString?.split("-")?.map(Number);
+      const startDate = new Date(year, month - 1, day);
 
-    // Get the current date
-    const currentDate = new Date();
+      // Get the current date
+      const currentDate = new Date();
 
-    // Calculate the difference in years, months, and days
-    let years = currentDate.getFullYear() - startDate.getFullYear();
-    let months = currentDate.getMonth() - startDate.getMonth();
-    let days = currentDate.getDate() - startDate.getDate();
+      // Calculate the difference in years, months, and days
+      let years = currentDate.getFullYear() - startDate.getFullYear();
+      let months = currentDate.getMonth() - startDate.getMonth();
+      let days = currentDate.getDate() - startDate.getDate();
 
-    // Adjust for negative days
-    if (days < 0) {
-      months--;
-      const previousMonth = new Date(
-        currentDate.getFullYear(),
-        currentDate.getMonth(),
-        0
-      );
-      days += previousMonth.getDate();
+      // Adjust for negative days
+      if (days < 0) {
+        months--;
+        const previousMonth = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth(),
+          0
+        );
+        days += previousMonth.getDate();
+      }
+
+      // Adjust for negative months
+      if (months < 0) {
+        years--;
+        months += 12;
+      }
+
+      return `${years} tahun ${months} bulan ${days} hari`;
     }
-
-    // Adjust for negative months
-    if (months < 0) {
-      years--;
-      months += 12;
-    }
-
-    return `${years} tahun ${months} bulan ${days} hari`;
   }
 
   return "";
