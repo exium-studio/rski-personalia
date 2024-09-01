9cecffde2af3c0463f9742762979cbb628fde0b1
sulenq <whyimustwriteit@gmail.com>
2024-09-01 21:14:09 +0700

    fix bug masa kerja

--- src/lib/calculateMasaKerjaFromTanggalMasuk.ts
+++ src/lib/calculateMasaKerjaFromTanggalMasuk.ts
@@ -2,7 +2,7 @@ import isDatePassed from "./isDatePassed";
 
 export default function calculateMasaKerjaFromTanggalMasuk(dateString: string) {
   if (dateString) {
-    if (isDatePassed(dateString, true)) {
+    if (!isDatePassed(dateString, true)) {
       return "-";
     } else {
       // Parse the input date string (format: d-m-y)
