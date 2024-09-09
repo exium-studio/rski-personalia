8b6bef7b9fe7e316277bd70adf0ace9bc3d94668
sulenq <whyimustwriteit@gmail.com>
2024-09-09 18:51:45 +0700

    styling

--- src/components/independent/DashboardJenisKelamin.tsx
+++ src/components/independent/DashboardJenisKelamin.tsx
@@ -44,109 +44,113 @@ export default function DashboardJenisKelamin({ ...props }: Props) {
 
   return (
     <>
-      {error && (
-        <>
-          {notFound && <NoData minH={"300px"} />}
-
-          {!notFound && (
-            <Center my={"auto"} minH={"300px"}>
-              <Retry loading={loading} retry={retry} />
-            </Center>
-          )}
-        </>
+      {loading && (
+        <Skeleton
+          flex={"1 1 0"}
+          borderRadius={12}
+          h={dashboardItemHeight}
+          minW={dashboardItemMinWidth}
+        />
       )}
 
-      {!error && (
-        <>
-          {loading && (
-            <Skeleton
-              flex={"1 1 0"}
-              borderRadius={12}
-              h={dashboardItemHeight}
-              minW={dashboardItemMinWidth}
-            />
+      {!loading && (
+        <VStack
+          flex={"1 1 0"}
+          align={"stretch"}
+          bg={bodyColor}
+          borderRadius={12}
+          minW={dashboardItemMinWidth}
+          p={responsiveSpacing}
+          gap={0}
+          h={dashboardItemHeight}
+          {...props}
+        >
+          <Text fontWeight={600}>Jenis Kelamin</Text>
+          <Text fontSize={14} opacity={0.6}>
+            Karyawan saat ini
+          </Text>
+
+          {error && (
+            <>
+              {notFound && <NoData minH={"300px"} />}
+
+              {!notFound && (
+                <Center my={"auto"} minH={"300px"}>
+                  <Retry loading={loading} retry={retry} />
+                </Center>
+              )}
+            </>
           )}
 
-          {!loading && data && (
-            <VStack
-              flex={"1 1 0"}
-              align={"stretch"}
-              bg={bodyColor}
-              borderRadius={12}
-              minW={dashboardItemMinWidth}
-              p={responsiveSpacing}
-              gap={0}
-              h={dashboardItemHeight}
-              {...props}
-            >
-              <Text fontWeight={600}>Jenis Kelamin</Text>
-              <Text fontSize={14} opacity={0.6}>
-                Karyawan saat ini
-              </Text>
+          {!error && (
+            <>
+              {data && (
+                <>
+                  <Wrap m={"auto"} spacing={10}>
+                    <VStack
+                      flex={"1 1 0"}
+                      my={responsiveSpacing}
+                      position={"relative"}
+                    >
+                      <VStack w={"100%"} className="doughnutChartContainer">
+                        <ChartDoughnut labels={labels} datasets={datasets} />
+                      </VStack>
 
-              <Wrap m={"auto"} spacing={10}>
-                <VStack
-                  flex={"1 1 0"}
-                  my={responsiveSpacing}
-                  position={"relative"}
-                >
-                  <VStack w={"100%"} className="doughnutChartContainer">
-                    <ChartDoughnut labels={labels} datasets={datasets} />
-                  </VStack>
+                      <Text
+                        position={"absolute"}
+                        left={"50%"}
+                        top={"50%"}
+                        transform={"translate(-50%, -50%)"}
+                        fontSize={48}
+                        opacity={0.6}
+                      >
+                        %
+                      </Text>
+                    </VStack>
 
-                  <Text
-                    position={"absolute"}
-                    left={"50%"}
-                    top={"50%"}
-                    transform={"translate(-50%, -50%)"}
-                    fontSize={48}
-                    opacity={0.6}
-                  >
-                    %
-                  </Text>
-                </VStack>
+                    <VStack
+                      flex={"1 1 0"}
+                      align={"stretch"}
+                      justify={"center"}
+                      maxW={"240px"}
+                      mx={"auto"}
+                    >
+                      <HStack gap={4}>
+                        <Box
+                          borderRadius={"full"}
+                          w={"10px"}
+                          h={"10px"}
+                          bg={"orange.200"}
+                        />
 
-                <VStack
-                  flex={"1 1 0"}
-                  align={"stretch"}
-                  justify={"center"}
-                  maxW={"240px"}
-                  mx={"auto"}
-                >
-                  <HStack gap={4}>
-                    <Box
-                      borderRadius={"full"}
-                      w={"10px"}
-                      h={"10px"}
-                      bg={"orange.200"}
-                    />
+                        <Text>Pria</Text>
 
-                    <Text>Pria</Text>
+                        <Text pl={2} ml={"auto"}>
+                          {data.persen_laki_laki}%
+                        </Text>
+                      </HStack>
 
-                    <Text pl={2} ml={"auto"}>
-                      {data.persen_laki_laki}%
-                    </Text>
-                  </HStack>
+                      <HStack gap={4}>
+                        <Box
+                          borderRadius={"full"}
+                          w={"10px"}
+                          h={"10px"}
+                          bg={"purple.400"}
+                        />
 
-                  <HStack gap={4}>
-                    <Box
-                      borderRadius={"full"}
-                      w={"10px"}
-                      h={"10px"}
-                      bg={"purple.400"}
-                    />
+                        <Text>Wanita</Text>
 
-                    <Text>Wanita</Text>
-
-                    <Text pl={2} ml={"auto"}>
-                      {data.persen_perempuan}%
-                    </Text>
-                  </HStack>
-                </VStack>
-              </Wrap>
-            </VStack>
+                        <Text pl={2} ml={"auto"}>
+                          {data.persen_perempuan}%
+                        </Text>
+                      </HStack>
+                    </VStack>
+                  </Wrap>
+                </>
+              )}
+            </>
           )}
-        </>
+        </VStack>
       )}
     </>
   );
--- src/components/independent/DashboardStatusKaryawan.tsx
+++ src/components/independent/DashboardStatusKaryawan.tsx
@@ -47,120 +47,124 @@ export default function DashboardStatusKaryawan({ ...props }: Props) {
 
   return (
     <>
-      {error && (
-        <>
-          {notFound && <NoData minH={"300px"} />}
-
-          {!notFound && (
-            <Center my={"auto"} minH={"300px"}>
-              <Retry loading={loading} retry={retry} />
-            </Center>
-          )}
-        </>
+      {loading && (
+        <Skeleton
+          flex={"1 1 0"}
+          borderRadius={12}
+          h={dashboardItemHeight}
+          minW={dashboardItemMinWidth}
+        />
       )}
 
-      {!error && (
-        <>
-          {loading && (
-            <Skeleton
-              flex={"1 1 0"}
-              borderRadius={12}
-              h={dashboardItemHeight}
-              minW={dashboardItemMinWidth}
-            />
+      {!loading && (
+        <VStack
+          flex={"1 1 0"}
+          align={"stretch"}
+          bg={bodyColor}
+          borderRadius={12}
+          minW={dashboardItemMinWidth}
+          p={responsiveSpacing}
+          gap={0}
+          h={dashboardItemHeight}
+          {...props}
+        >
+          <Text fontWeight={600}>Status Karyawan</Text>
+          <Text fontSize={14} opacity={0.6}>
+            Karyawan saat ini
+          </Text>
+
+          {error && (
+            <>
+              {notFound && <NoData minH={"300px"} />}
+
+              {!notFound && (
+                <Center my={"auto"} minH={"300px"}>
+                  <Retry loading={loading} retry={retry} />
+                </Center>
+              )}
+            </>
           )}
 
-          {!loading && data && (
-            <VStack
-              flex={"1 1 0"}
-              align={"stretch"}
-              bg={bodyColor}
-              borderRadius={12}
-              minW={dashboardItemMinWidth}
-              p={responsiveSpacing}
-              gap={0}
-              h={dashboardItemHeight}
-              {...props}
-            >
-              <Text fontWeight={600}>Status Karyawan</Text>
-              <Text fontSize={14} opacity={0.6}>
-                Karyawan saat ini
-              </Text>
+          {!error && (
+            <>
+              {data && (
+                <>
+                  <Wrap m={"auto"} spacing={10}>
+                    <VStack
+                      flex={"1 1 0"}
+                      my={responsiveSpacing}
+                      position={"relative"}
+                    >
+                      <VStack w={"100%"} className="doughnutChartContainer">
+                        <ChartDoughnut labels={labels} datasets={datasets} />
+                      </VStack>
 
-              <Wrap m={"auto"} spacing={10}>
-                <VStack
-                  flex={"1 1 0"}
-                  my={responsiveSpacing}
-                  position={"relative"}
-                >
-                  <VStack w={"100%"} className="doughnutChartContainer">
-                    <ChartDoughnut labels={labels} datasets={datasets} />
-                  </VStack>
+                      <Text
+                        position={"absolute"}
+                        left={"50%"}
+                        top={"50%"}
+                        transform={"translate(-50%, -50%)"}
+                        fontSize={48}
+                        opacity={0.6}
+                      >
+                        N
+                      </Text>
+                    </VStack>
 
-                  <Text
-                    position={"absolute"}
-                    left={"50%"}
-                    top={"50%"}
-                    transform={"translate(-50%, -50%)"}
-                    fontSize={48}
-                    opacity={0.6}
-                  >
-                    N
-                  </Text>
-                </VStack>
+                    <VStack
+                      flex={"1 1 0"}
+                      align={"stretch"}
+                      justify={"center"}
+                      maxW={"240px"}
+                      mx={"auto"}
+                    >
+                      <HStack gap={4}>
+                        <Box
+                          borderRadius={"full"}
+                          w={"10px"}
+                          h={"10px"}
+                          bg={"orange.200"}
+                        />
 
-                <VStack
-                  flex={"1 1 0"}
-                  align={"stretch"}
-                  justify={"center"}
-                  maxW={"240px"}
-                  mx={"auto"}
-                >
-                  <HStack gap={4}>
-                    <Box
-                      borderRadius={"full"}
-                      w={"10px"}
-                      h={"10px"}
-                      bg={"orange.200"}
-                    />
+                        <Text>Tetap</Text>
 
-                    <Text>Tetap</Text>
+                        <Text ml={"auto"}>{data[0].jumlah_karyawan}</Text>
+                      </HStack>
 
-                    <Text ml={"auto"}>{data[0].jumlah_karyawan}</Text>
-                  </HStack>
+                      <HStack gap={4}>
+                        <Box
+                          borderRadius={"full"}
+                          w={"10px"}
+                          h={"10px"}
+                          bg={"purple.400"}
+                        />
+                        <Text>Kontrak</Text>
 
-                  <HStack gap={4}>
-                    <Box
-                      borderRadius={"full"}
-                      w={"10px"}
-                      h={"10px"}
-                      bg={"purple.400"}
-                    />
-                    <Text>Kontrak</Text>
+                        <Text pl={2} ml={"auto"}>
+                          {data[1].jumlah_karyawan}
+                        </Text>
+                      </HStack>
 
-                    <Text pl={2} ml={"auto"}>
-                      {data[1].jumlah_karyawan}
-                    </Text>
-                  </HStack>
+                      <HStack gap={4}>
+                        <Box
+                          borderRadius={"full"}
+                          w={"10px"}
+                          h={"10px"}
+                          bg={"green.400"}
+                        />
+                        <Text>Training</Text>
 
-                  <HStack gap={4}>
-                    <Box
-                      borderRadius={"full"}
-                      w={"10px"}
-                      h={"10px"}
-                      bg={"green.400"}
-                    />
-                    <Text>Training</Text>
-
-                    <Text pl={2} ml={"auto"}>
-                      {data[2].jumlah_karyawan}
-                    </Text>
-                  </HStack>
-                </VStack>
-              </Wrap>
-            </VStack>
+                        <Text pl={2} ml={"auto"}>
+                          {data[2].jumlah_karyawan}
+                        </Text>
+                      </HStack>
+                    </VStack>
+                  </Wrap>
+                </>
+              )}
+            </>
           )}
-        </>
+        </VStack>
       )}
     </>
   );
