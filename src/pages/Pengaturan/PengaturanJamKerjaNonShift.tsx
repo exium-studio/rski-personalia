9d03b36cc5e58a4b7459759aaf51a254f6b022c7
sulenq <whyimustwriteit@gmail.com>
2024-10-11 13:26:30 +0700

    api jam kerja non shift

--- src/pages/Pengaturan/PengaturanJamKerjaNonShift.tsx
+++ src/pages/Pengaturan/PengaturanJamKerjaNonShift.tsx
@@ -3,7 +3,6 @@ import {
   Center,
   FormControl,
   FormErrorMessage,
-  FormLabel,
   HStack,
   Text,
   useToast,
@@ -12,29 +11,24 @@ import {
 import { useFormik } from "formik";
 import { useEffect, useRef, useState } from "react";
 import * as yup from "yup";
-import StringInput from "../../components/dependent/input/StringInput";
 import TimePickerModal from "../../components/dependent/input/TimePickerModal";
 import Retry from "../../components/dependent/Retry";
-import RequiredForm from "../../components/form/RequiredForm";
 import Skeleton from "../../components/independent/Skeleton";
 import CContainer from "../../components/wrapper/CContainer";
+import PermissionTooltip from "../../components/wrapper/PermissionTooltip";
 import { useLightDarkColor } from "../../constant/colors";
-import req from "../../lib/req";
+import days from "../../constant/days";
 import { responsiveSpacing } from "../../constant/sizes";
-import useRenderTrigger from "../../hooks/useRenderTrigger";
+import useAuth from "../../global/useAuth";
 import useDataState from "../../hooks/useDataState";
 import formatDate from "../../lib/formatDate";
-import useAuth from "../../global/useAuth";
 import isHasPermissions from "../../lib/isHasPermissions";
-import PermissionTooltip from "../../components/wrapper/PermissionTooltip";
+import req from "../../lib/req";
 
-export default function PengaturanJamKerjaNonShift() {
-  // SX
-  const lightDarkColor = useLightDarkColor();
-
+const JamKerjaItem = ({ id, day }: any) => {
   const [updateLoading, setUpdateLoading] = useState<boolean>(false);
   const toast = useToast();
-  const { rt, setRt } = useRenderTrigger();
+  const [rt, setRt] = useState(false);
 
   const formik = useFormik({
     validateOnChange: false,
@@ -50,13 +44,14 @@ export default function PengaturanJamKerjaNonShift() {
     }),
     onSubmit: (values, { resetForm }) => {
       const payload = {
+        _method: "patch",
         nama: values.nama,
         jam_from: values.jam_from,
         jam_to: values.jam_to,
       };
       setUpdateLoading(true);
       req
-        .post(`/api/rski/dashboard/pengaturan/non-shift`, payload)
+        .post(`/api/rski/dashboard/pengaturan/non-shift/${id}`, payload)
         .then((r) => {
           if (r.status === 200) {
             toast({
@@ -84,12 +79,14 @@ export default function PengaturanJamKerjaNonShift() {
         .finally(() => {
           setUpdateLoading(false);
         });
+
+      console.log(day);
     },
   });
 
   const { error, data, loading, retry } = useDataState<any>({
     initialData: undefined,
-    url: "/api/rski/dashboard/pengaturan/non-shift/1",
+    url: `/api/rski/dashboard/pengaturan/non-shift/${id}`,
     dependencies: [],
   });
 
@@ -106,15 +103,7 @@ export default function PengaturanJamKerjaNonShift() {
   const editPermission = isHasPermissions(userPermissions, [100]);
 
   return (
-    <CContainer
-      p={responsiveSpacing}
-      pb={responsiveSpacing}
-      bg={lightDarkColor}
-      borderRadius={12}
-      flex={"1 1 600px"}
-      h={"100%"}
-      overflowY={"auto"}
-    >
+    <>
       {error && (
         <>
           <Center my={"auto"} minH={"300px"}>
@@ -122,117 +111,141 @@ export default function PengaturanJamKerjaNonShift() {
           </Center>
         </>
       )}
-
       {!error && (
         <>
           {loading && (
-            <CContainer flex={1} gap={4}>
-              <Skeleton h={"60px"} />
+            <CContainer flex={1} gap={2}>
+              <Skeleton h={"24px"} w={"100px"} />
+
               <HStack gap={4}>
-                <Skeleton h={"60px"} />
-                <Skeleton h={"60px"} />
+                <Skeleton h={"40px"} />
+                <Skeleton h={"40px"} flexShrink={0} w={"102px"} />
               </HStack>
 
-              <HStack mt={"auto"} justify={"space-between"}>
-                <Skeleton h={"16px"} maxW={"200px"} />
-
-                <Skeleton ml={"auto"} w={"120px"} h={"40px"} />
-              </HStack>
+              <Skeleton h={"12px"} maxW={"200px"} />
             </CContainer>
           )}
 
           {!loading && (
             <>
-              <form id="jamKerjaNonShiftForm" onSubmit={formik.handleSubmit}>
-                <FormControl mb={4} isInvalid={!!formik.errors.jam_from}>
-                  <FormLabel>
-                    Nama
-                    <RequiredForm />
-                  </FormLabel>
-                  <StringInput
-                    name="nama"
-                    onChangeSetter={(input) => {
-                      formik.setFieldValue("nama", input);
-                    }}
-                    inputValue={formik.values.nama}
-                    placeholder="Nama Jam Kerja"
-                  />
-                  <FormErrorMessage>
-                    {formik.errors.jam_from as string}
-                  </FormErrorMessage>
-                </FormControl>
-
-                <FormLabel>
-                  Jam Kerja
-                  <RequiredForm />
-                </FormLabel>
-
-                <Wrap spacing={4} mb={4}>
-                  <FormControl
-                    flex={"1 1"}
-                    isInvalid={!!formik.errors.jam_from}
-                  >
-                    <TimePickerModal
-                      id="tambah-shift-jam-from-modal"
-                      name="jam_from"
-                      onConfirm={(input) => {
-                        formik.setFieldValue("jam_from", input);
-                      }}
-                      inputValue={formik.values.jam_from}
-                      isError={!!formik.errors.jam_from}
-                    />
-
-                    <FormErrorMessage>
-                      {formik.errors.jam_from as string}
-                    </FormErrorMessage>
-                  </FormControl>
-
-                  <Text mt={"5px"} textAlign={"center"}>
-                    -
+              <form
+                id={`jamKerjaNonShiftForm-${id}`}
+                onSubmit={formik.handleSubmit}
+              >
+                <CContainer>
+                  <Text fontWeight={500} mb={2}>
+                    {day}
                   </Text>
 
-                  <FormControl flex={"1 1"} isInvalid={!!formik.errors.jam_to}>
-                    <TimePickerModal
-                      id="tambah-shift-jam-to-modal"
-                      name="jam_to"
-                      onConfirm={(input) => {
-                        formik.setFieldValue("jam_to", input);
-                      }}
-                      inputValue={formik.values.jam_to}
-                      isError={!!formik.errors.jam_to}
-                    />
+                  <HStack gap={responsiveSpacing}>
+                    <CContainer>
+                      {/* <FormLabel>
+                        Jam Kerja
+                        <RequiredForm />
+                      </FormLabel> */}
 
-                    <FormErrorMessage>
-                      {formik.errors.jam_to as string}
-                    </FormErrorMessage>
-                  </FormControl>
-                </Wrap>
+                      <Wrap spacing={4}>
+                        <FormControl
+                          flex={"1 1"}
+                          isInvalid={!!formik.errors.jam_from}
+                        >
+                          <TimePickerModal
+                            id="tambah-shift-jam-from-modal"
+                            name="jam_from"
+                            onConfirm={(input) => {
+                              formik.setFieldValue("jam_from", input);
+                            }}
+                            inputValue={formik.values.jam_from}
+                            isError={!!formik.errors.jam_from}
+                          />
+
+                          <FormErrorMessage>
+                            {formik.errors.jam_from as string}
+                          </FormErrorMessage>
+                        </FormControl>
+
+                        <Text mt={"5px"} textAlign={"center"}>
+                          -
+                        </Text>
+
+                        <FormControl
+                          flex={"1 1"}
+                          isInvalid={!!formik.errors.jam_to}
+                        >
+                          <TimePickerModal
+                            id="tambah-shift-jam-to-modal"
+                            name="jam_to"
+                            onConfirm={(input) => {
+                              formik.setFieldValue("jam_to", input);
+                            }}
+                            inputValue={formik.values.jam_to}
+                            isError={!!formik.errors.jam_to}
+                          />
+
+                          <FormErrorMessage>
+                            {formik.errors.jam_to as string}
+                          </FormErrorMessage>
+                        </FormControl>
+                      </Wrap>
+                    </CContainer>
+
+                    <PermissionTooltip permission={editPermission}>
+                      <Button
+                        className="btn-ap clicky"
+                        colorScheme="ap"
+                        ml={"auto"}
+                        mt={"auto"}
+                        mb={0}
+                        type="submit"
+                        form={`jamKerjaNonShiftForm-${id}`}
+                        isLoading={updateLoading}
+                        isDisabled={!editPermission}
+                      >
+                        Simpan
+                      </Button>
+                    </PermissionTooltip>
+                  </HStack>
+
+                  <Text fontSize={12} opacity={0.4} mt={2}>
+                    Terakhir diperbarui : {formatDate(data.updated_at)}
+                  </Text>
+                </CContainer>
               </form>
 
-              <HStack justify={"space-between"} mt={"auto"}>
-                <Text opacity={0.4}>
-                  Terakhir diperbarui : {formatDate(data.updated_at)}
-                </Text>
-
-                <PermissionTooltip permission={editPermission}>
-                  <Button
-                    className="btn-ap clicky"
-                    colorScheme="ap"
-                    ml={"auto"}
-                    mt={"auto"}
-                    type="submit"
-                    form="jamKerjaNonShiftForm"
-                    isLoading={updateLoading}
-                    isDisabled={!editPermission}
-                  >
-                    Simpan
-                  </Button>
-                </PermissionTooltip>
-              </HStack>
+              {/* <HStack justify={"space-between"} mt={"auto"}></HStack> */}
             </>
           )}
         </>
       )}
+    </>
+  );
+};
+
+export default function PengaturanJamKerjaNonShift() {
+  // SX
+  const lightDarkColor = useLightDarkColor();
+
+  return (
+    <CContainer
+      py={responsiveSpacing}
+      pb={responsiveSpacing}
+      bg={lightDarkColor}
+      borderRadius={12}
+      flex={"1 1 600px"}
+      h={"100%"}
+      overflowY={"auto"}
+      className="scrollY"
+    >
+      <CContainer
+        px={responsiveSpacing}
+        gap={responsiveSpacing}
+        overflowY={"auto"}
+        className="scrollY"
+      >
+        {days.map((day, i) => (
+          <JamKerjaItem key={i} id={i + 1} day={day} />
+        ))}
+      </CContainer>
     </CContainer>
   );
 }
