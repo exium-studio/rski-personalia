d0a157bbe6e80622da6a4fb6bb0c785315291f37
sulenq <fatwalinovera@gmail.com>
2025-05-08 08:52:13 +0700

    feat: export karyawan medis

---
+++ src/components/dependent/ExportKaryawanMedisModal.tsx
@@ -0,0 +1,175 @@
+import {
+  Button,
+  ButtonGroup,
+  ButtonProps,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import { RiUploadLine } from "@remixicon/react";
+import { useRef, useState } from "react";
+import { iconSize } from "../../constant/sizes";
+import useBackOnClose from "../../hooks/useBackOnClose";
+import backOnClose from "../../lib/backOnClose";
+import download from "../../lib/download";
+import req from "../../lib/req";
+import CContainer from "../wrapper/CContainer";
+import DisclosureHeader from "./DisclosureHeader";
+import FilterKaryawanMedis from "./FilterKaryawanMedis";
+
+interface Props extends ButtonProps {}
+
+export default function ExportKaryawanMedisModal({ ...props }: Props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  useBackOnClose(`export-modal-${1}`, isOpen, onOpen, onClose);
+  const initialRef = useRef(null);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const toast = useToast();
+
+  const defaultFilterConfig = {
+    masa_sip: undefined as any,
+    masa_str: undefined as any,
+  };
+  const [filterConfig, setFilterConfig] = useState<any>(defaultFilterConfig);
+
+  const handleExport = () => {
+    setLoading(true);
+
+    const url = `/api/rski/dashboard/karyawan/export-karyawan-medis`;
+
+    const payload = {
+      masa_sip: filterConfig.masa_sip,
+      masa_str: filterConfig.masa_str,
+    };
+
+    req
+      .post(url, payload, {
+        responseType: "blob",
+      })
+      .then((r) => {
+        if (r.status === 200) {
+          download(r.data, `Export Karyawan Medis`, "xls");
+        } else {
+          toast({
+            status: "error",
+            title:
+              "Terjadi kendala, silahkan periksa jaringan atau hubungi SIM RS",
+            isClosable: true,
+            position: "bottom-right",
+          });
+        }
+      })
+      .catch(async (e) => {
+        // Default error message
+        let errorMsg =
+          "Terjadi kendala, silahkan periksa jaringan atau hubungi SIM RS";
+
+        // Try to parse error blob if content-type is JSON
+        if (e?.response?.data instanceof Blob) {
+          try {
+            const contentType = e.response.headers["content-type"];
+            if (contentType && contentType.includes("application/json")) {
+              const text = await e.response.data.text();
+              const json = JSON.parse(text);
+              if (typeof json.message === "string") {
+                errorMsg = json.message;
+              }
+            }
+          } catch (err) {
+            console.error("Gagal parse blob jadi JSON:", err);
+          }
+        }
+
+        toast({
+          status: "error",
+          title: errorMsg,
+          isClosable: true,
+          position: "bottom-right",
+        });
+      })
+      .finally(() => {
+        backOnClose();
+        setLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <Button
+        minW={"120px"}
+        className="btn-outline clicky"
+        leftIcon={<Icon as={RiUploadLine} fontSize={iconSize} />}
+        pl={5}
+        onClick={onOpen}
+        {...props}
+      >
+        Export
+      </Button>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={() => {
+          backOnClose();
+        }}
+        initialFocusRef={initialRef}
+        isCentered
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader ref={initialRef}>
+            <DisclosureHeader
+              title={"Export Karyawan Medis"}
+              onClose={() => {}}
+            />
+          </ModalHeader>
+          <ModalBody>
+            <Text opacity={0.6}>
+              Apakah anda yakin akan export (excel) semua data pada tabel ini?
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <CContainer gap={2}>
+              <CContainer gap={2}>
+                <FilterKaryawanMedis
+                  id={"filter-kary-medis-export"}
+                  inputValue={filterConfig}
+                  onConfirm={setFilterConfig}
+                />
+              </CContainer>
+
+              <ButtonGroup>
+                <Button
+                  w={"100%"}
+                  className="btn-solid clicky"
+                  onClick={() => {
+                    backOnClose();
+                  }}
+                >
+                  Tidak
+                </Button>
+                <Button
+                  w={"100%"}
+                  className="btn-ap clicky"
+                  colorScheme="ap"
+                  isLoading={loading}
+                  onClick={handleExport}
+                >
+                  Ya
+                </Button>
+              </ButtonGroup>
+            </CContainer>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
--- src/pages/Karyawan/KaryawanMedis.tsx
+++ src/pages/Karyawan/KaryawanMedis.tsx
@@ -1,265 +1,16 @@
-import {
-  Button,
-  ButtonGroup,
-  Center,
-  Flex,
-  FormControl,
-  FormLabel,
-  HStack,
-  Icon,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  RangeSlider,
-  RangeSliderFilledTrack,
-  RangeSliderThumb,
-  RangeSliderTrack,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { RiEqualizer3Fill } from "@remixicon/react";
-import { useRef, useState } from "react";
-import DisclosureHeader from "../../components/dependent/DisclosureHeader";
-import ExportModal from "../../components/dependent/ExportModal";
+import { HStack } from "@chakra-ui/react";
+import { useState } from "react";
+import ExportKaryawanMedisModal from "../../components/dependent/ExportKaryawanMedisModal";
+import FilterKaryawanMedis from "../../components/dependent/FilterKaryawanMedis";
 import SearchComponent from "../../components/dependent/input/SearchComponent";
 import TabelKaryawanmedis from "../../components/dependent/TabelKaryawanmedis";
 import CContainer from "../../components/wrapper/CContainer";
 import CWrapper from "../../components/wrapper/CWrapper";
 import PermissionTooltip from "../../components/wrapper/PermissionTooltip";
 import { useLightDarkColor } from "../../constant/colors";
-import { iconSize, responsiveSpacing } from "../../constant/sizes";
+import { responsiveSpacing } from "../../constant/sizes";
 import useAuth from "../../global/useAuth";
-import useBackOnClose from "../../hooks/useBackOnClose";
-import backOnClose from "../../lib/backOnClose";
-import formatNumber from "../../lib/formatNumber";
 import isHasPermissions from "../../lib/isHasPermissions";
-
-const Filter = (props: any) => {
-  const { filterConfig, setFilterConfig } = props;
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  useBackOnClose("filter-masa-berakhir-sip-str", isOpen, onOpen, onClose);
-
-  const [localFilterConfig, setLocalFilterConfig] = useState<any | null>(
-    filterConfig
-  );
-  const initialRef = useRef(null);
-  const filterCount = () => {
-    let count = 0;
-    if (filterConfig?.masa_sip) count += 1;
-    if (filterConfig?.masa_str) count += 1;
-    return count;
-  };
-  const lightDarkColor = useLightDarkColor();
-
-  return (
-    <>
-      <Button
-        className="btn-outline clicky"
-        leftIcon={<Icon as={RiEqualizer3Fill} fontSize={iconSize} />}
-        flexShrink={0}
-        pl={5}
-        onClick={() => {
-          onOpen();
-          // setLocalFilterConfig(filterKaryawan);
-          // setClear(false);
-        }}
-      >
-        <HStack>
-          {filterCount() && (
-            <Center
-              position={"absolute"}
-              right={"-6px"}
-              top={"-6px"}
-              flexShrink={0}
-              minW={"20px"}
-              h={"20px"}
-              borderRadius={"full"}
-              bg={"p.500"}
-              ml={"auto"}
-            >
-              <Text color={lightDarkColor} fontSize={12} fontWeight={600}>
-                {formatNumber(filterCount())}
-              </Text>
-            </Center>
-          )}
-
-          <Text>Filter Sisa Masa Berlaku</Text>
-        </HStack>
-      </Button>
-
-      <Modal
-        isOpen={isOpen}
-        onClose={backOnClose}
-        initialFocusRef={initialRef}
-        isCentered
-        // scrollBehavior="inside"
-      >
-        <ModalOverlay />
-
-        <ModalContent>
-          <ModalHeader ref={initialRef}>
-            <DisclosureHeader title={"Filter Sisa Masa Berlaku"} />
-
-            <ModalBody>
-              <FormControl mb={4}>
-                <FormLabel>{`SIP`}</FormLabel>
-
-                <Center>
-                  <Text fontSize={18} fontWeight={500}>
-                    {localFilterConfig.masa_sip
-                      ? `0 - ${localFilterConfig.masa_sip} bulan`
-                      : `Semua`}
-                  </Text>
-                </Center>
-
-                <CContainer pr={4}>
-                  <RangeSlider
-                    min={0}
-                    max={12}
-                    step={1}
-                    defaultValue={[0, localFilterConfig.masa_sip || 0]}
-                    colorScheme="ap"
-                    onChange={(val: number[]) => {
-                      const [, end] = val;
-
-                      setLocalFilterConfig((ps: any) => ({
-                        ...ps,
-                        masa_sip: end,
-                      }));
-                    }}
-                    value={[0, localFilterConfig.masa_sip || 0]}
-                  >
-                    <RangeSliderTrack
-                      bg={"var(--divider2)"}
-                      w={"calc(100% + 15px) !important"}
-                    >
-                      <RangeSliderFilledTrack />
-                    </RangeSliderTrack>
-
-                    <RangeSliderThumb index={0} bg={"p.600"} />
-                    <RangeSliderThumb index={1} bg={"p.600"} />
-                  </RangeSlider>
-                </CContainer>
-
-                <Flex justify="space-between">
-                  {[0, 3, 6, 9, 12].map((val, i) => {
-                    const valid = i !== 0 && i !== 4;
-
-                    return (
-                      <Text
-                        key={val}
-                        w={"18px"}
-                        textAlign={"center"}
-                        fontSize="sm"
-                        whiteSpace={"nowrap"}
-                        opacity={valid ? 1 : 0}
-                      >
-                        {val}
-                      </Text>
-                    );
-                  })}
-                </Flex>
-              </FormControl>
-
-              <FormControl mb={4}>
-                <FormLabel>{`STR`}</FormLabel>
-                <Center>
-                  <Text fontSize={18} fontWeight={500}>
-                    {localFilterConfig.masa_str
-                      ? `0 - ${localFilterConfig.masa_str} bulan`
-                      : `Semua`}
-                  </Text>
-                </Center>
-
-                <CContainer pr={4}>
-                  <RangeSlider
-                    min={0}
-                    max={12}
-                    step={1}
-                    defaultValue={[0, localFilterConfig.masa_str || 0]}
-                    colorScheme="ap"
-                    onChange={(val: number[]) => {
-                      const [, end] = val;
-
-                      setLocalFilterConfig((ps: any) => ({
-                        ...ps,
-                        masa_str: end,
-                      }));
-                    }}
-                    value={[0, localFilterConfig.masa_str || 0]}
-                  >
-                    <RangeSliderTrack
-                      bg={"var(--divider2)"}
-                      w={"calc(100% + 15px) !important"}
-                    >
-                      <RangeSliderFilledTrack />
-                    </RangeSliderTrack>
-
-                    <RangeSliderThumb index={0} bg={"p.600"} />
-                    <RangeSliderThumb index={1} bg={"p.600"} />
-                  </RangeSlider>
-                </CContainer>
-
-                <Flex justify="space-between">
-                  {[0, 3, 6, 9, 12].map((val, i) => {
-                    const valid = i !== 0 && i !== 4;
-
-                    return (
-                      <Text
-                        key={val}
-                        w={"18px"}
-                        textAlign={"center"}
-                        fontSize="sm"
-                        whiteSpace={"nowrap"}
-                        opacity={valid ? 1 : 0}
-                      >
-                        {val}
-                      </Text>
-                    );
-                  })}
-                </Flex>
-              </FormControl>
-            </ModalBody>
-
-            <ModalFooter>
-              <ButtonGroup w={"100%"}>
-                <Button
-                  w={"50%"}
-                  className="btn-solid clicky"
-                  onClick={() => {
-                    setLocalFilterConfig({
-                      ...localFilterConfig,
-                      masa_sip: undefined,
-                      masa_str: undefined,
-                    });
-                  }}
-                >
-                  Clear
-                </Button>
-
-                <Button
-                  onClick={() => {
-                    setFilterConfig(localFilterConfig);
-                    backOnClose();
-                  }}
-                  w={"50%"}
-                  colorScheme="ap"
-                  className="btn-ap clicky"
-                >
-                  Terapkan
-                </Button>
-              </ButtonGroup>
-            </ModalFooter>
-          </ModalHeader>
-        </ModalContent>
-      </Modal>
-    </>
-  );
-};
 
 export default function KaryawanMedis() {
   // SX
@@ -306,21 +57,17 @@ export default function KaryawanMedis() {
               inputValue={filterConfig.search}
             />
 
-            <Filter
-              filterConfig={filterConfig}
-              setFilterConfig={setFilterConfig}
+            <FilterKaryawanMedis
+              id={"filter-kary-medis-index"}
+              inputValue={filterConfig}
+              onConfirm={setFilterConfig}
             />
 
             <PermissionTooltip
               permission={exportPermissions}
               boxProps={{ w: "fit-content" }}
             >
-              <ExportModal
-                url="/api/rski/dashboard/perusahaan/diklat-eksternal/export"
-                title="Export Diklat Eksternal"
-                downloadFileName="Data Diklat Eksternal"
-                isDisabled={!exportPermissions}
-              />
+              <ExportKaryawanMedisModal />
             </PermissionTooltip>
           </HStack>
 
