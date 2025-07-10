15a492584067eac16fdb26ff46e515cb9e1089c0
sulenq <fatwalinovera@gmail.com>
2025-07-10 19:24:12 +0700

    refactor: wip filter masa diklat

--- src/components/dependent/FilterMasaDiklat.tsx
+++ src/components/dependent/FilterMasaDiklat.tsx
@@ -2,6 +2,7 @@ import {
   Button,
   ButtonGroup,
   Center,
+  Checkbox,
   Flex,
   FormControl,
   FormLabel,
@@ -17,11 +18,12 @@ import {
   RangeSliderFilledTrack,
   RangeSliderThumb,
   RangeSliderTrack,
+  Switch,
   Text,
   useDisclosure,
 } from "@chakra-ui/react";
 import useBackOnClose from "../../hooks/useBackOnClose";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLightDarkColor } from "../../constant/colors";
 import { RiEqualizer3Fill } from "@remixicon/react";
 import { iconSize } from "../../constant/sizes";
@@ -36,6 +38,7 @@ const FilterMasaDiklat = (props: any) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   useBackOnClose(id || "filter-masa-diklat", isOpen, onOpen, onClose);
 
+  const [more, setMore] = useState(false);
   const [localFilterConfig, setLocalFilterConfig] = useState<any | null>(
     inputValue
   );
@@ -47,6 +50,16 @@ const FilterMasaDiklat = (props: any) => {
     return count;
   };
   const lightDarkColor = useLightDarkColor();
+
+  useEffect(() => {
+    if (more) {
+      setLocalFilterConfig({
+        ...localFilterConfig,
+        less_than: undefined,
+        more_than: 20,
+      });
+    }
+  }, [more]);
 
   return (
     <>
@@ -101,164 +114,92 @@ const FilterMasaDiklat = (props: any) => {
               <FormControl>
                 <FormLabel>{`Masa Diklat`}</FormLabel>
 
-                <Center>
-                  <Text fontSize={18} fontWeight={500}>
-                    {localFilterConfig.less_than || localFilterConfig.more_than
-                      ? `${localFilterConfig.more_than || 0} - ${
-                          localFilterConfig.less_than || 0
-                        } jam`
-                      : `Semua`}
-                  </Text>
-                </Center>
-
-                <CContainer pr={4}>
-                  <RangeSlider
-                    min={0}
-                    max={20}
-                    step={1}
-                    defaultValue={[
-                      localFilterConfig.more_than || 0,
-                      localFilterConfig.less_than || 0,
-                    ]}
-                    colorScheme="ap"
-                    onChange={(val: number[]) => {
-                      const [start, end] = val;
-
-                      setLocalFilterConfig((ps: any) => ({
-                        ...ps,
-                        more_than: start,
-                        less_than: end,
-                      }));
-                    }}
-                    value={[
-                      localFilterConfig.more_than || 0,
-                      localFilterConfig.less_than || 0,
-                    ]}
-                  >
-                    <RangeSliderTrack
-                      bg={"var(--divider2)"}
-                      w={"calc(100% + 15px) !important"}
-                    >
-                      <RangeSliderFilledTrack />
-                    </RangeSliderTrack>
-
-                    <RangeSliderThumb index={0} bg={"p.600"} />
-                    <RangeSliderThumb index={1} bg={"p.600"} />
-                  </RangeSlider>
-                </CContainer>
-
-                <Flex justify="space-between">
-                  {[0, 5, 10, 15, 20].map((val, i) => {
-                    const valid = i !== 0 && i !== 4;
-
-                    return (
-                      <Text
-                        key={val}
-                        w={"18px"}
-                        textAlign={"center"}
-                        fontSize="sm"
-                        whiteSpace={"nowrap"}
-                        opacity={valid ? 1 : 0}
-                      >
-                        {val}
-                      </Text>
-                    );
-                  })}
-                </Flex>
-              </FormControl>
-
-              {/* <SimpleGrid columns={[1, null, 2]} gap={4}>
-                <FormControl>
-                  <FormLabel>{`Lebih dari/sama (>=)`}</FormLabel>
-
+                <CContainer opacity={more ? 0.5 : 1}>
                   <Center>
                     <Text fontSize={18} fontWeight={500}>
-                      {localFilterConfig.masa_sip
-                        ? `<= ${localFilterConfig.masa_sip} bulan`
+                      {localFilterConfig.less_than ||
+                      localFilterConfig.more_than
+                        ? `${localFilterConfig.more_than || 0} - ${
+                            localFilterConfig.less_than || 0
+                          } jam`
                         : `Semua`}
                     </Text>
                   </Center>
 
-                  <RangeSlider
-                    min={0}
-                    max={12}
-                    step={1}
-                    defaultValue={[0, localFilterConfig.masa_sip || 0]}
-                    colorScheme="ap"
-                    onChange={(val: number[]) => {
-                      const [, end] = val;
+                  <CContainer pr={4}>
+                    <RangeSlider
+                      min={0}
+                      max={20}
+                      step={1}
+                      defaultValue={[
+                        localFilterConfig.more_than || 0,
+                        localFilterConfig.less_than || 0,
+                      ]}
+                      colorScheme="ap"
+                      onChange={(val: number[]) => {
+                        const [start, end] = val;
 
-                      setLocalFilterConfig((ps: any) => ({
-                        ...ps,
-                        masa_sip: end,
-                      }));
-                    }}
-                    value={[0, localFilterConfig.masa_sip || 0]}
-                  >
-                    <RangeSliderTrack bg={"var(--divider2)"}>
-                      <RangeSliderFilledTrack />
-                    </RangeSliderTrack>
+                        setLocalFilterConfig((ps: any) => ({
+                          ...ps,
+                          more_than: start,
+                          less_than: end,
+                        }));
+                      }}
+                      value={[
+                        localFilterConfig.more_than || 0,
+                        localFilterConfig.less_than || 0,
+                      ]}
+                    >
+                      <RangeSliderTrack
+                        bg={"var(--divider2)"}
+                        w={"calc(100% + 15px) !important"}
+                      >
+                        <RangeSliderFilledTrack />
+                      </RangeSliderTrack>
 
-                    <RangeSliderThumb index={0} bg={"p.600"} />
-                    <RangeSliderThumb index={1} bg={"p.600"} />
-                  </RangeSlider>
+                      <RangeSliderThumb index={0} bg={"p.600"} />
+                      <RangeSliderThumb index={1} bg={"p.600"} />
+                    </RangeSlider>
+                  </CContainer>
 
                   <Flex justify="space-between">
-                    {[0, 3, 6, 9, 12].map((val, i) => {
+                    {[0, 5, 10, 15, 20].map((val, i) => {
                       const valid = i !== 0 && i !== 4;
 
                       return (
                         <Text
                           key={val}
-                          w={"0px"}
+                          w={"18px"}
                           textAlign={"center"}
                           fontSize="sm"
                           whiteSpace={"nowrap"}
                           opacity={valid ? 1 : 0}
-                          ml={1}
                         >
                           {val}
                         </Text>
                       );
                     })}
                   </Flex>
+                </CContainer>
 
-                  <InputGroup>
-                    <InputRightElement mr={3}>jam</InputRightElement>
-                    <NumberInput
-                      onChangeSetter={(input) => {
-                        setLocalFilterConfig({
-                          ...localFilterConfig,
-                          more_than: input,
-                        });
-                      }}
-                      inputValue={localFilterConfig?.more_than}
-                      name="more_than"
-                      placeholder="Lebih dari"
-                      pr={"56px"}
-                    />
-                  </InputGroup>
-                </FormControl>
-
-                <FormControl>
-                  <FormLabel>{`Kurang dari/sama (<=)`}</FormLabel>
-                  <InputGroup>
-                    <InputRightElement mr={3}>jam</InputRightElement>
-                    <NumberInput
-                      onChangeSetter={(input) => {
-                        setLocalFilterConfig({
-                          ...localFilterConfig,
-                          less_than: input,
-                        });
-                      }}
-                      inputValue={localFilterConfig?.less_than}
-                      name="less_than"
-                      placeholder="Kurang dari"
-                      pr={"56px"}
-                    />
-                  </InputGroup>
-                </FormControl>
-              </SimpleGrid> */}
+                <Checkbox
+                  colorScheme="ap"
+                  mt={8}
+                  onChange={(e) => {
+                    setMore(e.target.checked);
+                    if (!e.target.checked) {
+                      setLocalFilterConfig({
+                        ...localFilterConfig,
+                        less_than: undefined,
+                        more_than: undefined,
+                      });
+                    }
+                  }}
+                  isChecked={more}
+                >
+                  Lebih dari 20 jam
+                </Checkbox>
+              </FormControl>
             </ModalBody>
 
             <ModalFooter>
